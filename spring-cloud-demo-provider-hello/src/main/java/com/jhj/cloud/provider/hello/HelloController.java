60c73c990990dc12e8d90626ef98e7a70f51de7f
zhangbo <363230482@qq.com>
2018-05-04 17:30:23 +0800

    zuul

--- spring-cloud-demo-provider-hello/src/main/java/com/jhj/cloud/provider/hello/HelloController.java
+++ spring-cloud-demo-provider-hello/src/main/java/com/jhj/cloud/provider/hello/HelloController.java
@@ -4,19 +4,31 @@ package com.jhj.cloud.provider.hello;
  * @author zhangbo
  */
 
+import java.util.concurrent.TimeUnit;
+
 import com.jhj.cloud.commons.dto.RestResultDto;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping(value = "/hello")
 public class HelloController {
     
-    @RequestMapping(value = "/hello", produces = "application/json;charset=UTF-8")
-    public RestResultDto hello() {
-        log.info("---------- hello ----------");
+    @GetMapping(value = "/get")
+    public RestResultDto hello(String body) {
+        log.info("---------- hello get: {}", body);
+        return RestResultDto.succeed();
+    }
+    
+    @PostMapping(value = "/post")
+    public RestResultDto post(String body) throws InterruptedException {
+        log.info("---------- hello post: {}", body);
+        TimeUnit.SECONDS.sleep(2);
         return RestResultDto.succeed();
     }
     
