b152fc98a191b1f9068fa5dcd39a1f26b9cc5d70
zhangbo <363230482@qq.com>
2018-06-13 15:26:18 +0800

    spring cloud config server

--- spring-cloud-demo-config-server/src/main/java/com/jhj/cloud/config/ConfigServerApplication.java
+++ spring-cloud-demo-config-server/src/main/java/com/jhj/cloud/config/ConfigServerApplication.java
@@ -2,11 +2,10 @@ package com.jhj.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
